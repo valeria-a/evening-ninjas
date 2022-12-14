5baf33bc08054374db7a265dbfe8258e44d3a6a6
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-14 19:18:37 +0200

    lesson 11 eve

---
+++ lesson1/lesson_code.py
@@ -0,0 +1,40 @@
+# result = 5 + 6
+# print(result)
+#
+#
+# num = 10
+# print(result * num)
+#
+# user_input = input("Insert your input: ")
+# print(user_input)
+#
+# print(type(num))
+# num1 = result / num
+# print(num1)
+# print(type(num1))
+# print(type(user_input))
+
+# year = input("Insert your birth year: ")
+# year_as_int = int(year)
+# age = 2022 - year_as_int
+# print("You are", age, "years old")
+# my_text = f"You are {age} years old"
+# my_text = "You are {age} years old"
+
+# print(f"You are {2022 - int(input('Insert a year: '))} years old")
+
+# word = "Sun"
+# word1 = 'Sun'
+# word2 = "She said: \"The weather is great\""
+# word3 = 'She said: "The weather is great"'
+# word4 = "Hello\nworld"
+# word5 = "Hello\tworld"
+# word6 = """My name is Valeria
+# I'm 35 years old
+# I live in Netanya
+# """
+# print(word6)
+#
+
+
+
