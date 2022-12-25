f67f48ce9925a554acbc266c761d520c7d43198d
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2022-12-25 20:05:58 +0200

    various

--- lesson4/mutability.py
+++ lesson4/mutability.py
@@ -1,1 +1,40 @@
-# built-in funciton: id, is
+# built-in function: id, is
+
+# num = 5
+# print(id(num))
+# num1 = 6
+# print(id(num1))
+# w = "hello"
+# print(id(w))
+# print(id(w[0]))
+# print(id(w[1]))
+# w[2] = 'f'
+
+# l1 = [1, 2, 3]
+# l1[2] = 5
+# print(id(l1[2]))
+
+# w1 = "apple"
+# w2 = "apple"
+#
+# print(id(w1), id(w2))
+# print(id(w1[0]))
+#
+# i1 = input("insert: ")
+# i2 = input("insert2: ")
+# print(id(i1), id(i2))
+
+# fruits = ['apple', 'pear', 'ananas']
+# a = fruits[0]
+# fruits[0] = "banana"
+# print(a)
+# print(fruits)
+
+# products = [['tomato', 'potato', 'onion', 'cabbage'],
+#             ['apple', 'pear', 'ananas'],
+#             ['cherry', 'blueberry', 'raspberry', 'strawberry']]
+#
+# vegs = products[0]
+# vegs.append('cucumber')
+# print(vegs)
+# print(products)
---
+++ lesson4/tuples_code.py
@@ -0,0 +1,39 @@
+# my_tuple = tuple()
+# t2 = (2, 4, 6, 2)
+# print(my_tuple)
+# print(t2)
+# print(t2[2])
+# # t2[1] = 9
+# seasons = ("summer", "autumn", "winter", "spring")
+# print("summer" in seasons)
+# print(len(seasons))
+#
+# for s in seasons:
+#     print(s.upper())
+
+# a = (2, 3)
+# print(a)
+# num1, num2 = (2, 3)
+# print(num1)
+
+# head, *tail = (1, 2, 3, 4, 5)
+# print(head)
+# print(tail)
+
+# *num1, num2 = 1, 2, 3, 4
+# print(num1)
+# print(num2)
+
+# a, *b, c, d = 1, 2, 3, 4, 5
+# print(b)
+# not working
+# *a, b, *c = 1, 2,3, 4,5
+
+# n1, *n2, n3 = ['apple', 56, "y", True]
+# print(n2)
+
+# hours, minutes = "17:30".split(":")
+# print(minutes)
+
+# l = [2,3,4]
+# "tyty".endswith(("y", "r", "fghu"))
