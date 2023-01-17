2e8a55044004d0e067dfce1c35defd9ad5b84e86
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-01-17 16:35:08 +0200

    eve lesson 10

---
+++ lesson10/cat_dog_poly.py
@@ -0,0 +1,48 @@
+class Cat:
+    def __init__(self, cat_type, color, name):
+        self.cat_type = cat_type
+        self.color = color
+        self.name = name
+
+    def make_sound(self):
+        print("Meow")
+
+
+class Dog:
+    def __init__(self, dog_type, color, name):
+        self.dog_type = dog_type
+        self.color = color
+        self.name = name
+
+    def make_sound(self):
+        print("Wof")
+
+
+cat = Cat('russian blue', 'grey-blue', 'Oliver')
+cat2 = Cat('persian', 'white', 'Mitzi')
+dog = Dog('doberman', 'black', 'Bruno')
+dog2 = Dog('bulldog', 'white-and-brown', 'Max')
+dog3 = Dog('chihuahua', 'red', 'Daisy')
+
+# cat.make_sound()
+# dog.make_sound()
+#
+# # print(cat.name, dog.name)
+#
+# animals_list = [cat, dog, dog2, cat2, dog3]
+# for animal in animals_list:
+#     print(animal.name)
+#     animal.make_sound()
+
+
+
+
+
+
+
+
+
+
+
+
+
