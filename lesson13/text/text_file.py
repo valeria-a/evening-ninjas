fd16596432354ea81252a456936ef8188117c33d
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-02-01 21:29:35 +0200

    eve lesson 13

--- lesson13/text/text_file.py
+++ lesson13/text/text_file.py
@@ -1,5 +1,8 @@
+import csv
+import json
 import os.path
 from abc import ABC, abstractmethod
+from io import TextIOWrapper
 
 
 class TextFileError(Exception):
@@ -16,45 +19,79 @@ class TextFile(ABC):
             raise FileNotFoundError()
 
         _, ext = os.path.splitext(file_path)
+        # if ext not in ('txt', '.json', '.csv'):
         if ext != self.get_file_ext():
             raise UnsupportedFileType()
 
-        self.file_path = file_path
+        self._file_path = file_path
 
     def get_file_size(self):
-        pass
-
+        return os.path.getsize(self._file_path)
 
     def get_content(self):
-        pass
+        with open(self._file_path, 'r') as f:
+            ret_val = self._read_file_content(f)
+        return ret_val
+
+    @abstractmethod
+    def _read_file_content(self, f: TextIOWrapper):
+        raise NotImplementedError()
 
     @abstractmethod
     def get_file_ext(self):
-        pass
+        raise NotImplementedError()
 
 
 class CsvFile(TextFile):
 
-    def __init__(self, file_path: str, delimeter: str = ","):
+    def __init__(self, file_path: str, delimiter: str = ","):
         super().__init__(file_path)
-        self.delimeter = delimeter
+        self._delimiter = delimiter
+
+    def _read_file_content(self, f: TextIOWrapper):
+        return list(csv.DictReader(f, delimiter=self._delimiter))
 
     def get_file_ext(self):
         return '.csv'
 
 
-
 class JsonFile(TextFile):
+
+    def _read_file_content(self, f: TextIOWrapper):
+        return json.load(f)
+
     def get_file_ext(self):
         return '.json'
 
 
 class TxtFile(TextFile):
+
+    def _read_file_content(self, f: TextIOWrapper):
+        return f.read()
+
     def get_file_ext(self):
         return '.txt'
+
 
 
 if __name__ == '__main__':
     # my_file = TxtFile('/t/t/y.txt')
     # my_file.get_file_size()
-    TextFile('/text_file.py')
+    txt_file = TxtFile('/Users/valeria/src/evening-ninjas/lesson12/files/data/alice_in_wonderland.txt')
+    csv_file = CsvFile('/Users/valeria/src/evening-ninjas/lesson12/files/data/AAPL.csv')
+    json_file = JsonFile('/Users/valeria/src/evening-ninjas/lesson12/files/data/example_2.json')
+
+    # txt_file + csv_file
+    # txt_file + json_file
+    # t1 = '/a/b/my.txt'
+    # t2 = 'c/d/sun.txt'
+    # new_f = t1 + t2 # /a/b/my_sun.txt
+    # # csv_file + json_file
+    # txt_file + txt_file #
+    # csv_file + csv_file
+    # a, b, c
+    # b, a, c, d
+    #
+
+    # for i in (t, c, j):
+    #     print(i.get_content())
