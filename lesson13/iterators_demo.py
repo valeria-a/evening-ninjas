e2817f95302725b61184e09e5921159bd692d452
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-02-04 20:49:17 +0200

    eve lesson 14

---
+++ lesson13/iterators_demo.py
@@ -0,0 +1,31 @@
+# # drinks = ['coffee', 'tea', 'juice', 'coke']
+# drinks = {'a': 1, 'b':2, 'c':3}
+#
+# for d in drinks:
+#     print(d)
+#
+# # for k, v in drinks.items():
+# #     print(k, v)
+#
+# drinks_iterator = iter(drinks.items())
+# print(type(drinks_iterator))
+# val = next(drinks_iterator)
+# print(val)
+# val = next(drinks_iterator)
+# print(val)
+# print(next(drinks_iterator))
+# print(next(drinks_iterator))
+# print(next(drinks_iterator))
+
+# class MyClass:
+#     # pass
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         pass
+#
+#
+# m = MyClass()
+# for i in m:
+#     print(i)
