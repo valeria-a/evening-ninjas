c2e15af9e82968a6aaefaf84daba4a133c341389
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2023-02-08 18:09:53 +0200

    eve lesson 15

--- lesson15/lesson_code/utils.py
+++ lesson15/lesson_code/utils.py
@@ -2,9 +2,6 @@ import math
 import time
 
 
-def performance_log():
-    pass
-
 def greeting(orig_func):
 
     def wrapping_function(*args, **kwargs):
@@ -15,6 +12,34 @@ def greeting(orig_func):
 
     return wrapping_function
 
+
+def performance_log(ms=False):
+    def f1(orig_func):
+        def f2(*args, **kwargs):
+            start = time.time()
+            ret_val = orig_func(*args, **kwargs)
+            end = time.time()
+            res = end - start
+            if ms:
+                res *= 1000
+            print(f"Function {orig_func.__name__} took {res} seconds")
+            return ret_val
+
+        return f2
+
+    return f1
+
+# def performance_log(orig_func):
+#
+#     def wrapping_function(*args, **kwargs):
+#         start = time.time()
+#         ret_val = orig_func(*args, **kwargs)
+#         end = time.time()
+#         print(f"Function {orig_func.__name__} took {end-start} seconds")
+#         return ret_val
+#
+#     return wrapping_function
+
 @greeting
 def factorial():
     num = int(input("Please insert a number: "))
@@ -23,17 +48,22 @@ def factorial():
 
 # factorial = greeting(factorial)
 
-@performance_log
+
 def capitalize():
     txt = input("Please insert a text to capitalize: ")
     print(f"Your capitalized text is: {txt.capitalize()}")
 
 # capitalize = my_dec(capitalize)
 
+@performance_log(ms=True)
 def convert_from_farenheit():
     temp_F = float(input("Please insert a temperature in °F: "))
     temp_C = (temp_F - 32) * (5/9)
     print(f"{temp_F} in °F is {temp_C} °C")
+
+# convert_from_farenheit = performance_log(ms=True)(convert_from_farenheit)
+
+
 
 
 @greeting
@@ -43,7 +73,7 @@ def convert_from_celcius():
     print(f"{temp_C} in °C is {temp_F} °F")
 
 
-@greeting
+@performance_log(ms=True)
 def convert_from_str(s: str):
     return int(s)
 
@@ -54,5 +84,7 @@ if __name__ == '__main__':
     # convert_from_celcius()
     # greeting_decorator(convert_from_farenheit)()
     # capitalize()
-    num = convert_from_str('4')
-    print(num)
+    # num = convert_from_str('4')
+    # print(num)
+    # convert_from_farenheit()
+    print(convert_from_str('554757567567'))
